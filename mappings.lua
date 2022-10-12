65d40a5408e1ba7cb9f32675b5baa6a2175c575c
Hans Tognon <learningway403@gmail.com>
2022-10-12 09:54:58 +0100

    Revert "up"
    This reverts commit 1e9f9a41e595c7fb20c43d2882d1fa177abfe249.

--- mappings.lua
+++ mappings.lua
@@ -1,6 +1,6 @@
 return {
   n = {
-    ["<leader>lA"] = { "<cmd>CodeActionMenu<cr>", desc = "CodeActionMenu" },
+    ["<leader>la"] = { "<cmd>CodeActionMenu<cr>", desc = "CodeActionMenu" },
     ["<leader>lI"] = { "<cmd>Mason<cr>", desc = "Mason" },
     ["<leader>bu"] = { "<cmd>DBUIToggle<cr>", desc = "DB UI Toggle" },
     ["<leader>bf"] = { "<cmd>DBUIFindBuffer<cr>", desc = "DB UI Find buffer" },
