203a1f6ea30a7272dc02a03e3e485ba7689c0b1d
Hans Tognon <learningway403@gmail.com>
2023-04-08 00:44:27 +0100

    chore: update

--- plugins/flutter.lua
+++ plugins/flutter.lua
@@ -2,6 +2,10 @@ return {
   {
     "akinsho/flutter-tools.nvim",
     ft = { "dart" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
     opts = {
       server = require("astronvim.utils.lsp").config "rust_analyzer",
     },
