75ce666aad2e6d79e5fb34cfe51949015c6b854a
Hans Tognon <learningway403@gmail.com>
2023-04-08 23:40:57 +0100

    chore: update

--- plugins/init.lua
+++ plugins/init.lua
@@ -13,6 +13,7 @@ return {
     "folke/todo-comments.nvim",
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() require("todo-comments").setup {} end,
   },
   {
     "kristijanhusak/vim-dadbod-ui",
