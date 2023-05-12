9ffff2059d63e7b913f7262db08febeda4b99a7f
Hans Tognon <learningway403@gmail.com>
2023-05-12 07:27:36 +0100

    chore: new vibes new theme

--- plugins/themes.lua
+++ plugins/themes.lua
@@ -1,26 +1,26 @@
 return {
-  {
-    "uloco/bluloco.nvim",
-    lazy = false,
-    priority = 1500,
-    dependencies = { "rktjmp/lush.nvim" },
-    config = function()
-      require("bluloco").setup {
-        style = "auto",
-        transparent = false,
-        italics = true,
-        terminal = true,
-        guicursor = true,
-      }
-    end,
-  },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  -- {
+  --   "uloco/bluloco.nvim",
+  --   lazy = false,
+  --   priority = 1500,
+  --   dependencies = { "rktjmp/lush.nvim" },
+  --   config = function()
+  --     require("bluloco").setup {
+  --       style = "auto",
+  --       transparent = false,
+  --       italics = true,
+  --       terminal = true,
+  --       guicursor = true,
+  --     }
+  --   end,
+  -- },
   -- { 'frenzyexists/aquarium-vim' },
   -- {
   --   "rose-pine/neovim",
   --   as = "rose-pine",
   --   config = function() require("rose-pine").setup { dark_variant = "moon" } end,
   -- },
-  -- { "uloco/bluloco.nvim", requires = {"rktjmp/lush.nvim"}},
   -- { "arcticicestudio/nord-vim" },
   -- { 'sainnhe/edge' },
   -- { "shaunsingh/solarized.nvim" },
