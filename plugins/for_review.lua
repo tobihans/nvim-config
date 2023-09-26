a5bbed1aa8284e26413e8a8e33f8b9a63855eb12
Hans Tognon <learningway403@gmail.com>
2023-09-26 10:24:48 +0100

    chore: latest addition

--- plugins/for_review.lua
+++ plugins/for_review.lua
@@ -11,4 +11,13 @@ return {
     event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
     config = function() require("symbol-usage").setup() end,
   },
+  {
+    "mikesmithgh/kitty-scrollback.nvim",
+    enabled = false,
+    lazy = true,
+    cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    -- version = '^1.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function() require("kitty-scrollback").setup() end,
+  },
 }
