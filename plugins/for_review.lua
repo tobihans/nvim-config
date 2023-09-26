a9da5b2e9bda473f6b6f09e0ca5d8c4e4d69fd31
Hans Tognon <learningway403@gmail.com>
2023-09-26 23:48:28 +0100

    fix: properly disable plugins

--- plugins/for_review.lua
+++ plugins/for_review.lua
@@ -1,13 +1,13 @@
 return {
   {
     "chrisgrieser/nvim-puppeteer",
-    enable = false,
+    enabled = false,
     dependencies = "nvim-treesitter/nvim-treesitter",
     lazy = false, -- plugin lazy-loads itself
   },
   {
     "Wansmer/symbol-usage.nvim",
-    enable = false,
+    enabled = false,
     event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
     config = function() require("symbol-usage").setup() end,
   },
