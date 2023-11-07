92d4455bcc04c9cb134b27960959a53c4deeb9d7
Hans Tognon <git@tobihans.space>
2023-11-07 09:24:59 +0100

    chore: update editor
    Learning to use quickfix for better S&R and diagnostic setup

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,9 +1,9 @@
 return {
+  { "AstroNvim/astrocommunity" },
   { "p00f/clangd_extensions.nvim" },
   { "mattn/emmet-vim", lazy = false },
   { "kaarmu/typst.vim", ft = "typst" },
   { "wakatime/vim-wakatime", lazy = false },
-  { "mg979/vim-visual-multi", lazy = false },
   { "kylechui/nvim-surround", lazy = false },
   { "folke/zen-mode.nvim", cmd = "ZenMode" },
   { "tiagovla/scope.nvim", lazy = false, priority = 1500 },
@@ -16,13 +16,13 @@ return {
   {
     "folke/drop.nvim",
     event = "VimEnter",
-    config = function() require("drop").setup { max = 25, screensaver = false } end,
+    opts = { max = 25, screensaver = false },
   },
   {
     "folke/todo-comments.nvim",
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function() require("todo-comments").setup {} end,
+    config = true,
   },
   {
     "kkoomen/vim-doge",
@@ -30,5 +30,24 @@ return {
     build = ":call doge#install()",
     init = function() vim.g.doge_enable_mappings = 0 end,
   },
-  { "AstroNvim/astrocommunity" }
+  {
+    "mg979/vim-visual-multi",
+    lazy = false,
+    init = function()
+      vim.g.VM_default_mappings = 1
+
+      -- Default
+      -- vim.g.VM_leader = { ["default"] = "\\", ["visual"] = "\\", ["buffer"] = "z" }
+
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-n>",
+        ["Find Subword Under"] = "<C-n>",
+        ["Select Cursor Down"] = "<M-C-Down>",
+        ["Select Cursor Up"] = "<M-C-Up>",
+        -- Enable undo/redo experimental feature
+        ["Undo"] = "u",
+        ["Redo"] = "<C-r>",
+      }
+    end,
+  },
 }
---
+++ plugins/quickfix.lua
@@ -0,0 +1,2 @@
+-- TODO: Configure this to suit my needs later
+return { "kevinhwang91/nvim-bqf", ft = "qf" }
