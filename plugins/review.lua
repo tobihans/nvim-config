1717b91bb044c69cb095efbda3d6f31840389861
Hans Tognon <git@tobihans.space>
2023-11-14 09:19:59 +0100

    chore: new plugins to review

--- plugins/review.lua
+++ plugins/review.lua
@@ -1,15 +1,10 @@
 -- A list of plugins that I want to review and decide if I want to keep them or not.
 return {
   {
-    "chrisgrieser/nvim-puppeteer",
-    enabled = false,
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    lazy = false, -- plugin lazy-loads itself
-  },
-  {
-    "Wansmer/symbol-usage.nvim",
-    enabled = false,
-    event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
-    config = function() require("symbol-usage").setup() end,
+    "topaxi/gh-actions.nvim",
+    cmd = "GhActions",
+    dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+    opts = {},
+    config = true,
   },
 }
