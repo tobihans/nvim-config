2316124bdd476e88c629e3a29a0924d93180b39a
Hans Tognon <git@tobihans.space>
2023-11-19 23:28:35 +0100

    feat(discord): add discord presence plugin
    This will be toggled based on my mood :)

---
+++ plugins/presence.lua
@@ -0,0 +1,14 @@
+return {
+  "andweeb/presence.nvim",
+  lazy = false,
+  enabled = false,
+  opts = {
+    buttons = false,
+    blacklist = {
+      "pitch-cardinal-coding",
+      "private",
+      "winlife-hq",
+    },
+  },
+  config = true,
+}
