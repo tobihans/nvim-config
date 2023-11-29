c1bf2c237f7f2ba0ac784d51930abe8c7b6971be
Hans Tognon <git@tobihans.space>
2023-11-29 21:18:39 +0100

    feat: update statuscolumn components

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -1,17 +1,13 @@
+local status = require "astronvim.utils.status"
+
 return {
   {
     "rebelot/heirline.nvim",
     opts = function(_, opts)
-      local status = require "astronvim.utils.status"
-      -- components
-      local mode = status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }
-      local surround_mode = status.utils.surround({ "", "  " }, status.hl.mode_bg, mode, function() return true end)
-
-      opts.winbar = nil;
-
+      opts.winbar = nil
       opts.statusline = {
         hl = { fg = "fg", bg = "bg" },
-        surround_mode,
+        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
         status.component.git_branch(),
         status.component.file_info { filetype = {}, filename = false, file_modified = false },
         status.component.git_diff(),
@@ -23,7 +19,12 @@ return {
         status.component.treesitter(),
         status.component.nav(),
       }
-
+      opts.statuscolumn = {
+        status.component.signcolumn(),
+        status.component.numbercolumn(),
+        status.component.fill(),
+        status.component.foldcolumn(),
+      }
       return opts
     end,
   },
