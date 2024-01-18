328ef62fcdb439c146ea075d7c8f818db643d99a
Hans Tognon <git@tobihans.space>
2024-01-18 07:47:34 +0100

    chore: udpates tab separators

--- init.lua
+++ init.lua
@@ -14,7 +14,8 @@ local config = {
     separators = {
       -- https://github.com/ryanoasis/powerline-extra-symbols
       -- tab = { "\u{e0ba}", "\u{e0bc}" },
-      tab = { "\u{e0b2}", "\u{e0b0}" },
+      -- tab = { "\u{e0b2}\u{e0c6}", "\u{e0b0}" },
+      tab = { "\u{e0d4}", "\u{e0d2}" },
     },
     attributes = {
       mode = { bold = true },
